6eb06fb4eaabba85ef929bb00a9940dc04820271
ChiVi123 <nguyenhoangchivi@gmail.com>
2021-11-05 20:26:04 +0700

    Chuyển các thuộc tính sang file ConnectDB

--- CMSProject/src/main/java/nhom4/utilities/Common.java
+++ CMSProject/src/main/java/nhom4/utilities/Common.java
@@ -1,11 +1,5 @@
 package nhom4.utilities;
 
 public class Common {
-	/*
-	 * Sang: Nhat: "nhatnguyen1712"; Vi: "ef00tb@ll2022";
-	 */
-	public static final String LINKDB = "jdbc:mysql://localhost:3306/webfinal";
-	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String DB_USER = "root";
-	public static final String DB_PASS = "ef00tb@ll2022";
+	
 }

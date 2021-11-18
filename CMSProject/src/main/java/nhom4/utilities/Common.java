0b565b780efd2cddeea1b544b8124382ad4b92b9
ChiVi123 <nguyenhoangchivi@gmail.com>
2021-11-19 01:40:39 +0700

    tao constant kieu string co ten cac file .tiles

--- CMSProject/src/main/java/nhom4/utilities/Common.java
+++ CMSProject/src/main/java/nhom4/utilities/Common.java
@@ -1,5 +1,8 @@
 package nhom4.utilities;
 
 public class Common {
-	
+	//link *.tiles
+	public static final String HOME_TILES = "home.tiles"; //hiển thị view content
+	public static final String PROFILE_TILES = "edit-profile.tiles";
+	public static final String CONTENT_TILES = "form-content.tiles";
 }

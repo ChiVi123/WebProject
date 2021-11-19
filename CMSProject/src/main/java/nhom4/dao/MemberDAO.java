e2f4cc69bf98df200ea0053d2090dc4e55752f78
19110254@student.hcmute.edu.vn <19110254@student.hcmute.edu.vn>
2021-11-19 22:58:11 +0700

    them update users và validation cho edit-profile

--- CMSProject/src/main/java/nhom4/dao/MemberDAO.java
+++ CMSProject/src/main/java/nhom4/dao/MemberDAO.java
@@ -2,15 +2,32 @@ package nhom4.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import nhom4.models.Member;
 import nhom4.dao.ConnectDB;
 
-
 public class MemberDAO {
+	private static final String UPDATE_MEMBER = "UPDATE Member "
+			+ " SET Firstname = ?, Lastname = ?, Phone = ?, Description = ? WHERE id = ;";
 
+	public MemberDAO() {
+	}
+
+	ConnectDB connect = new ConnectDB();
+	public boolean updateContent(Member member) throws SQLException {
+		boolean rowUpdated;
+		try (Connection connection = connect.getConnection();
+				PreparedStatement statement = connection.prepareStatement(UPDATE_MEMBER);) {
+		/*	statement.setString(1, member.getFirstname());
+			statement.setString(2, member.getLastname());
+			statement.setString(3, member.getPhone());
+		   statement.setString(2, member.getDescription());
+			*/
+			statement.setInt(4, member.getId());
+			;
+			rowUpdated = statement.executeUpdate() > 0;
+		}
+		return rowUpdated;
+	}
 }
